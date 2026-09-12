2cda93cc53cb4321af3c96d91cc078c14e5a1653
hammon737 <hammon737@morrisville.edu>
2026-09-12 15:17:19 -0400

    Keyboard Input & if Statements
    I added if statements to mark when the player is using the WASD keys

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] float moveSpeed = 0.03f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        if (Keyboard.current.wKey.isPressed) 
+        {
+            Debug.Log("we are pushing forward");
+        }
+
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("we are pushing left");
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("we are pushing right");
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("we are pushing back");
+        }
+
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0,moveSpeed,0);
     }
