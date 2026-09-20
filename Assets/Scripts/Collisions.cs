e327f1016fb28c8d123145dc432e7f1b6c7c1b9f
hammon737 <hammon737@morrisville.edu>
2026-09-20 17:40:28 -0400

    Using Tags in Unity
    I created a Package and Customer tag that both give debug messages to the player for driving over them.

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
-    SpriteRenderer carRender;
-    void Start()
-    {
-        carRender = GetComponent<SpriteRenderer>();
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Crash!!!");
-
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("What was that??");
-        carRender.color = Color.pink;
+        if(collision.CompareTag("Package"))
+        {
+            Debug.Log("You picked up a package");
+        }
+
+        if (collision.CompareTag("Customer"))
+        {
+            Debug.Log("You delivered a package");
+        }
+
     }
 }
