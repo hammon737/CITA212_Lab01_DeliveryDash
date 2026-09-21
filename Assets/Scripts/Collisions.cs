e02f8ef5da975b5da6cb4fd8d54da967e2a7d81d
hammon737 <hammon737@morrisville.edu>
2026-09-21 09:56:14 -0400

    Introducing Bools
    I added a boolean string called "hasPackage" that detects whether or not the player picked up the package, since you cannot deliver a package that you did not have.

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
-    
+    bool hasPackage;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Package"))
         {
             Debug.Log("You picked up a package");
+            hasPackage = true;
         }
 
-        if (collision.CompareTag("Customer"))
+        if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("You delivered a package");
+            hasPackage = false;
         }
 
     }
