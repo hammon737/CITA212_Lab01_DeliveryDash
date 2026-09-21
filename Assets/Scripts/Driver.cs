3a0607d9897ed125a5c3dc2be2c059886bf7d71c
hammon737 <hammon737@morrisville.edu>
2026-09-21 11:07:36 -0400

    Boosts & Bumps
    When the player collects a boost object, their speed increases to 10. When they collide with a wall, their speed goes back to 0.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,7 +5,23 @@ using UnityEngine.InputSystem;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 200f;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float currentSpeed = 5f;
+    [SerializeField] float boostSpeed = 10f;
+    [SerializeField] float regularSpeed = 5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Boost"))
+        {
+            currentSpeed = boostSpeed;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed = regularSpeed;
+    }
 
     void Update()
     {
@@ -32,7 +48,7 @@ public class Driver : MonoBehaviour
             move = -1f;
         }
 
-        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float moveAmount = move * currentSpeed * Time.deltaTime;
         float steerAmount = steer * steerSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, steerAmount);
